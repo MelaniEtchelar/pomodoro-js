41959c3bcdb81051999924d9f58d930e1eb9bf45
Melani Etchelar <melanietchelar97@gmail.com>
2023-04-09 11:50:47 -0300

    seguimos con el codigo

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ let timer =  null;
 let timerBreak = null;
 let current = null;
 
+
 const bAdd = document.querySelector("#bAdd");
 const itTask = document.querySelector("#itTask");
 const form = document.querySelector("#form");
-
+const taskName = document.querySelector('#time #taskName');
 
 form.addEventListener("submit", e => {
     e.preventDefault();
@@ -46,6 +47,51 @@ function renderTask() {
 
  const tasksContainer = document.querySelector('#tasks');
  tasksContainer.innerHTML = html.join('');
+
+ const startButtons = document.querySelectorAll(".task .start-button");
+
+ startButtons.forEach(button => {
+    button.addEventListener('click', e => {
+        if(!timer){
+            const id = button.getAttribute('data-id');
+            startButtonHandler(id);
+            button.textContent = 'In progress ...'
+
+        }
+    });
+ });
     
 }
 
+function startButtonHandler(id) {
+    time = 25 * 60;
+    current = id;
+    const taskIndex = tasks.findIndex(task => task.id === id);
+    
+    taskName.textContent = tasks[taskIndex].title;
+
+    timer = setInterval(() => {
+        timeHandler(id);
+    }, 1000);
+}
+
+function timerHandler(id){
+    time--;
+    renderTime();
+
+    if(time===0){
+        clearInterval(timer);
+        current = null;
+        taskName.textContent = "";
+    }
+}
+
+function renderTime(){
+    const timeDiv = document.querySelector('#time #value');
+    const minutes = parseInt(time / 60);
+    const seconds = parseInt(time % 60);
+
+    timeDiv.textContent = `${minutes < 10 ? "0" : ""}${minutes}:${
+        seconds < 10 ? "0" : ""
+    }${seconds}`
+}
